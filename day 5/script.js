6adc907d0287da7ad18c1c00e94724a4c0f1be20
Rani <ranibhosale168@gmail.com>
2026-02-20 12:12:44 +0530

    day 5 desicion making system  added

---
+++ day 5/script.js
@@ -0,0 +1,79 @@
+// decision making statements 
+// if statement
+// if(condition){
+//     // code to be executed if condition is true
+// }
+
+// let marks = 85;
+// if (marks >= 90) {
+//     console.log("Grade A");
+// } 
+
+// if else statement
+// if(condition){
+//     // code to be executed if condition is true
+// } else {
+//     // code to be executed if condition is false
+// }
+
+// if (marks >= 90) {
+//     console.log("Grade A");
+// } else {
+//     console.log("Grade B");
+// }
+
+// let num= -10;
+// if(num >=0){
+//     console.log(num);
+// }
+
+
+// if else if statement
+// if(condition1){
+//     // code to be executed if condition1 is true
+// } else if(condition2){
+//     // code to be executed if condition2 is true
+// } else {
+//     // code to be executed if both condition1 and condition2 are false
+// }
+
+// let number = 0;
+// if (number === 0) {
+//     console.log("Number is zero");
+// } else if (number > 0) {
+//     console.log("Number is positive");
+// }
+//     else {
+//     console.log("Number is negative");
+// }
+
+
+let marks = 40;
+if (marks >= 90) {
+    console.log("exlent marks");
+}else if (marks >= 80) {
+    console.log(" very good marks");
+}else if (marks >= 70) {
+    console.log("good marks");
+}else if (marks >= 60) {
+    console.log("average marks");
+}else if (marks >= 50) {
+    console.log("below average marks");
+}else if (marks >= (40)) {
+    console.log("pdhai kar le bhai");
+}else if (marks <= 40) {
+    console.log("pdhai chodo bhai");
+}
+
+let number = 15;
+if ( number % 15 == 0) {
+    console.log("FizzBuzz");
+} 
+ else if (number%3 ==0){
+    console.log("FIzz");
+}
+else if (number%5 ==0){
+    console.log("Buzz");
+} else{
+    console.log(number);
+}
