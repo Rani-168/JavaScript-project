44036827d3edd2c2a687043231c84148bdb3f468
Rani <ranibhosale168@gmail.com>
2026-02-24 16:10:48 +0530

    js day7 while loop and do while loop   added

--- day 6/script.js
+++ day 6/script.js
@@ -13,51 +13,79 @@
 
 
 
-for(let num=2; num<=20; num+=2){
-    console.log(num);
-}
+// for(let num=2; num<=20; num+=2){
+//     console.log(num);
+// }
 
-for(let num=1; num<=100; num++){
-   if(num%3==0 && num%5==0){
-    console.log("fizzbuzz");
-   }    
-   else if(num%3==0){
-    console.log("fizz");
-   }
+// for(let num=1; num<=100; num++){
+//    if(num%3==0 && num%5==0){
+//     console.log("fizzbuzz");
+//    }    
+//    else if(num%3==0){
+//     console.log("fizz");
+//    }
 
-   else if(num%5==0){
-      console.log("buzz");
-   }
-   else{
-      console.log(num);
-   }
-} 
+//    else if(num%5==0){
+//       console.log("buzz");
+//    }
+//    else{
+//       console.log(num);
+//    }
+// } 
 
 
 
 
 // given number check if it is prime or not if it prime thrn print given number is prime  if it not then print given number is not prime
-let number=7;
-   if(number%2!=0 && number%3!=0 && number%5!=0){  
+let number=13;
+   if( number%2!=0 && number%3!=0 && number%5!=0 && number!=1){  
          console.log(number + " is a prime number");
         }
-      else{
-         console.log(number + " is not a prime number");
+      else if(number==2 || number==3 || number==5){
+         console.log(number + " is a prime number");
        
+      }
+      else {
+         console.log(number + " is not a prime number");
       }
 
      // OR
 
 
-let num=3;
+// let num=3;
 
-for(let i=2; i<num; i++){   //
-    if(num%i==0){
-        isPrime=false;
-        break;
-    }
-}
-    console.log(num + " is a prime number");
+// for(let i=2; i<num; i++){   //
+//     if(num%i==0){
+//         isPrime=false;
+//         break;
+//     }
+// }
+//     console.log(num + " is a prime number");
+
+
+// for loop when use when you know the number of iterations you want to perform. 
+// while loop when you don't know the number of iterations you want to perform.
+// do while loop when you want to execute a block of code at least once, and then repeat it as long as a specified condition is true.
+
+// // while loop
+// let count=1;
+// while(count<=10){
+//     console.log(count);
+//     count++;
+// }
+
+// // do while loop
+// let i=1;
+// do{
+//       console.log(i);
+//       i++;
+// }while(false){
+//    console.log("hii");
+// }
+
+
+
+
 
 
     
