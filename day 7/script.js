e5bb4d1dbfbf5b43fa6680a8c175264473470571
Rani <ranibhosale168@gmail.com>
2026-02-25 10:50:40 +0530

    js day7 practice set    added

---
+++ day 7/script.js
@@ -0,0 +1,178 @@
+// // sum of two numbers
+// let number= 4;
+// let number2= 2;
+
+// sum= number + number2;
+// console.log(sum);
+
+
+// // even or odd
+// let num= 5;
+// if(num % 2 === 0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+
+// // largest of three numbers
+
+// let a= 10;
+// let b= 20;
+// let c= 25;
+
+// if (a > b && a > c){
+//     console.log(a);
+// }
+// else if(b > a && b > c){
+//     console.log(b);
+// }
+// else{
+//     console.log(c);
+// }
+
+
+//simple calculater
+
+
+// let num1 = 4;
+// let operator = "+";
+// let num2 = 3;
+
+// let result;
+
+// if (operator === "+") {
+//    console.log(num1 + num2);
+// } else if (operator === "-") {
+//   console.log(num1 - num2);
+// } else if (operator === "*") {
+//  console.log(num1 * num2);
+// } else if (operator === "/") {
+//   if (num2 !== 0) {
+//     console.log(num1 / num2);
+//   } else {
+//     console.log("Error: Division by zero");
+//   }
+// } else {
+//   console.log("Invalid operator");
+// }
+
+// print number 1-10 by using loop
+
+// by for loop
+// for(let num=1; num<=10; num++){
+//     console.log(num);
+// }
+
+
+// let num = 1;
+// while(num<=10){
+    
+//     console.log(num);
+//    num++
+
+// }
+
+
+//multiplication table
+
+// let num = 3;
+// let mul;
+// for(let i=1; i<=10; i++){
+//     mul = num * i;
+//     console.log(num + " " + "×" +  " " + i +  " " + "=" + " " + mul);
+// }
+
+
+
+//Celsius to Fahrenheit
+
+// let cel = 1;
+// let Fahrenheit = (cel * 9/5) + 32;
+// console.log(Fahrenheit);
+
+
+
+//Find the Remainder
+
+// let num1 = 2;
+// let num2 = 3;
+// let rem = num1 % num2;
+// console.log(rem);
+
+
+//Compare Two Numbers
+
+// let num1 = 3;
+// let num2 = 3;
+// if(num1 > num2){
+//     console.log(num1);
+// }
+// else if(num1 === num2){
+//     console.log("equle");
+
+// }
+// else{
+//   console.log(num2);
+// }
+
+
+
+// find the given numder is  zero, positive, negative
+
+// let num1 = 0;
+// if(num1 >= 1){
+//     console.log("positive");
+// }
+// else if(num1 === 0){
+//     console.log("zero");
+
+// }
+// else{
+//   console.log("negative");
+// }
+
+
+
+//print even number between 1- 50
+
+// for(let num=1; num <=50; num++){
+//     if(num%2 === 0){
+//         console.log(num);
+//     }
+// }
+
+
+// repeat a string
+
+// let num = 3;
+// let string = "Hi";
+// let space = " ";
+// for(let i=1 ; i<=num ; i++){
+//     space += string;
+   
+// }
+//  console.log( space );
+
+
+
+//Sum of Array Elements
+
+
+let num = [1, 2];
+let sum = 0;
+
+for(let i=0 ; i<num.length ; i++){
+    sum += num.length;
+   console.log( sum);
+}
+ 
+
+
+
+
+
+
+
+
