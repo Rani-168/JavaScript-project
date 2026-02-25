52b5abd1b6909b0075dc91c657412085dd7e06e1
Rani <ranibhosale168@gmail.com>
2026-02-25 15:20:46 +0530

    js day8 added

---
+++ day 8/script.js
@@ -0,0 +1,29 @@
+// fuctions
+//1 pre-defined function // fuction which are already defined or pre-written function also called to predifed func.
+// eg. console.log();
+// Math.max();
+// .min();
+// .floor();
+// .ceil();
+// .random(); // between 0 to 1 dont give 0 and 1 (1 is excluded)
+//  let manNum = Math.max(10, 30, 40);
+//  console.log(manNum);
+
+// foolwing 3 fuction are available in browser 
+//1. alert("message"); // it wil not witten nothing // it written undefined
+//2. confirm("qusetion"); written you true or false
+//3. prompt("qusetion"); // it will return you string or null value
+
+
+//2 user-defined function 
+
+
+//accept number input from user and check if that number even or odd if even then show alert them given numeber even
+
+
+
+
+// take two number input num 1, num2, and display their sum
+
+
+
