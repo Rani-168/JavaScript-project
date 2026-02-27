0b89a74e2de9835fb4e4a0a3f3209821a461b1a8
Rani <ranibhosale168@gmail.com>
2026-02-27 18:59:13 +0530

    js day 10 js  added

---
+++ day 10/script.js
@@ -0,0 +1,31 @@
+// function ckeckPrime(){
+//   let num = prompt("Enter a number");
+//   let count =0;
+//   for(i=1; i<=num; i++){
+//     if(num%i===0){
+//         count++
+//     }
+
+//     } 
+//     if(count === 2){
+//         alert("prime");
+//     }else{
+//         alert("not prime");
+//     }
+
+   
+// }
+
+
+
+// function sumofCheck(){
+//     let num1= prompt("num1");
+    
+//     let num2 = prompt("num2");
+    
+//     alert(+num1 + +num2);
+
+// }
+
+
+
