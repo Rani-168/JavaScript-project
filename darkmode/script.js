e77d9b2bc310d128f89ce0cb1019761a78d89be1
Rani <ranibhosale168@gmail.com>
2026-03-04 14:59:46 +0530

    add id attribute to dark mode button and update icon switching logic

--- darkmode/script.js
+++ darkmode/script.js
@@ -1,8 +1,15 @@
 
 function chageDarkMode(){
    let dark = document.body;
+   let icon = document.getElementById("img");
    
     dark.classList.toggle("para");
+ if(dark.classList.contains("para")){
+       icon.src = "https://w7.pngwing.com/pngs/763/299/png-transparent-button-on-control-touch-interface-power-off.png";   // dark mode icon
+   } else {
+       icon.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStd2zLWAHSKoqvfQSkoaeiZUyMiHCGgaF5SA&s";  // light mode icon
+   }
+    
     
    
 }
