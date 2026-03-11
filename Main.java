3b8e53a39c6efd86f7c44db7a2094a784793ca19
Rani <ranibhosale168@gmail.com>
2026-03-11 19:22:07 +0530

    java code  added

--- Main.java
+++ Main.java
@@ -142,23 +142,64 @@
 
 
 
-import java.util.Scanner;
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         while(b!=0){
+//         int temp = b;
+//         b = a%b;
+//         a = temp;
+//         }
+//         System.out.println(a);
+//     }
+// }
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+// int arr[] = {1,2,2,3,4,4,5};
+
+// for(int i = 0; i < arr.length; i++){
+//     for(int j = i+1; j < arr.length; j++){
+//         if(arr[i] == arr[j]){
+//             arr[j] = -1;
+//         }
+//     }
+// }
+
+// for(int i = 0; i < arr.length; i++){
+//     if(arr[i] != -1)
+//         System.out.print(arr[i] + " ");
+// }
+//     }
+// }
 
 public class Main {
+
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        while(b!=0){
-        int temp = b;
-        b = a%b;
-        a = temp;
+        int arr[] = {1, 2, 3, 4, 4, 3, 6, 7, 2};
+        for(int i=0; i<arr.length; i++){
+            for(int j=1+i; j<arr.length; j++){
+                if(arr[i] == arr[j]){
+                    arr[j] = -1;
+                }
+            }
         }
-        System.out.println(a);
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != -1) 
+                 System.out.print(arr[i] + " ");            
+        }
     }
 }
+
 
 
 
