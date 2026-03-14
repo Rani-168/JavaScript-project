2e17486dba0315051f563a6b1ec1054e232c3882
Rani <ranibhosale168@gmail.com>
2026-03-14 14:07:34 +0530

    java code   added

--- Main.java
+++ Main.java
@@ -181,24 +181,24 @@
 //     }
 // }
 
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 4, 3, 6, 7, 2};
-        for(int i=0; i<arr.length; i++){
-            for(int j=1+i; j<arr.length; j++){
-                if(arr[i] == arr[j]){
-                    arr[j] = -1;
-                }
-            }
-        }
+//     public static void main(String[] args) {
+//         int arr[] = {1, 2, 3, 4, 4, 3, 6, 7, 2};
+//         for(int i=0; i<arr.length; i++){
+//             for(int j=1+i; j<arr.length; j++){
+//                 if(arr[i] == arr[j]){
+//                     arr[j] = -1;
+//                 }
+//             }
+//         }
 
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] != -1) 
-                 System.out.print(arr[i] + " ");            
-        }
-    }
-}
+//         for(int i = 0; i < arr.length; i++){
+//             if(arr[i] != -1) 
+//                  System.out.print(arr[i] + " ");            
+//         }
+//     }
+// }
 
 
 
