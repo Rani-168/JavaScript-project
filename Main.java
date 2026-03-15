0fe274c5c638d27f8ec81807d4d6c1a97d2391c1
Rani <ranibhosale168@gmail.com>
2026-03-15 22:01:13 +0530

    java code   added

--- Main.java
+++ Main.java
@@ -204,22 +204,22 @@
 
 
 
-public class Main {
-    public static void main(String[] args) {
-int arr[] = {1,2,2,3,4,9,5};
+// public class Main {
+//     public static void main(String[] args) {
+// int arr[] = {1,2,2,3,4,9,5};
 
- int sum =arr[0];
-for(int i=0; i<arr.length; i++){
+//  int sum =arr[0];
+// for(int i=0; i<arr.length; i++){
     
-        if(arr[i] > sum) {
-              sum = arr[i];
-        }
+//         if(arr[i] > sum) {
+//               sum = arr[i];
+//         }
           
     
-}
-System.err.println(sum);
-    }
-}
+// }
+// System.err.println(sum);
+//     }
+// }
 
 
 
