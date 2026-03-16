86284e2e06a43d6eb7a5f4302648415bdbdcec7a
Rani <ranibhosale168@gmail.com>
2026-03-16 19:17:31 +0530

    react code   added

---
+++ day 21/first-project/src/App.jsx
@@ -0,0 +1,11 @@
+import './FirstComponent'
+import './App.css'
+
+function App() {  // compoenet always start with fristlater capital
+  return <p>hiii
+     
+  </p>   // it can return  anly in parent element// in compenet each and evry opening tag should have its closing tag
+ 
+}
+
+export default App
---
+++ day 21/first-project/src/FirstComponent.jsx
@@ -0,0 +1,15 @@
+function FristComponent(){   // we can pass value as property
+    let city = "pune";
+    return (
+        <div>
+            <p>we live in {pune}</p>
+                            
+        </div>
+    )
+}
+
+export default FristComponent;
+
+//we cant use object directly in {} dirctly but we can use array
+
+// to add class we use "className" atribute.
