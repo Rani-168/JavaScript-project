0a072917afc22d925ce0bc8ead7878e14995bb29
Rani <ranibhosale168@gmail.com>
2026-04-08 21:04:20 +0530

    added java page code

--- ClientApp.java
+++ ClientApp.java
@@ -1,6 +1,6 @@
 public class ClientApp {
     public static void main(String[] args) {
-       Customer cus = new Customer(101, "Rani", 9876543210);
+        Customer cus = new Customer(101, "Doe", 1234567890, "rani@gmail.com");
          System.out.println(cus);
     }
 }
