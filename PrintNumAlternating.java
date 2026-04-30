66a93c387a7c781666f5fd332ad21db83935aa04
Rani <ranibhosale168@gmail.com>
2026-04-30 22:26:03 +0530

    added java1 code

--- PrintNumAlternating.java
+++ PrintNumAlternating.java
@@ -716,23 +716,23 @@
 // }
 
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class PrintNumAlternating{
-		public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        int n = sc.nextInt();
+// public class PrintNumAlternating{
+// 		public static void main(String[] args) {
+// 	        Scanner sc = new Scanner(System.in);
+// 	        int n = sc.nextInt();
 	      
-	        for(int i=1; i<=n; i++) {
-	        	  if(i%2 == 0) {
-	        		  System.out.print(-i + " ");
-	        	  }else {
-	        		  System.out.print(i + " ");
-	        	  }
-	        }
+// 	        for(int i=1; i<=n; i++) {
+// 	        	  if(i%2 == 0) {
+// 	        		  System.out.print(-i + " ");
+// 	        	  }else {
+// 	        		  System.out.print(i + " ");
+// 	        	  }
+// 	        }
 	        
 	        
-		}
-}
+// 		}
+// }
 
 
---
+++ palindromeNum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class palindromeNum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int original = num; 
+		int newValue=0;
+		while(num!=0) {
+			int digit = num % 10;
+			newValue = newValue * 10 + digit;
+			num = num/10;
+		}
+		if(original == newValue) {
+			System.out.print("it is palindrome Number");
+		}else {
+			System.out.print("it is Not palindrome Number");
+		}
+    
+	}	
+}
